756e0c2b46f948b25226aada8d8be22980477609
r567tw <r567tw@gmail.com>
2025-03-27 17:04:22 +0800

    Day 04

---
+++ Day04/app.js
@@ -0,0 +1,12 @@
+const arr = ["hello", "world", "how", "are", "you"];
+
+console.log(arr.length);
+console.log(arr.join("、"));
+console.log(arr.push("today"));
+console.log(arr.pop());
+console.log("==shift==");
+console.log(arr);
+console.log(arr.shift()); // 移除第一個元素
+console.log(arr);
+console.log("=======");
+console.log(arr.length);
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - Day 01: HelloWorld
 - Day 02: Guess Number
 - Day 03: For & While
+- Day 04: Array
